3edf85acf724653071db7e9274ce64e360e346b7
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-06 08:01:44 +0000

    v 1.4
    git-svn-id: https://plugins.svn.wordpress.org/otw-portfolio-light/tags/1.4@927618 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/otw-portfolio.js
@@ -0,0 +1,45 @@
+/* Scroll to Top Button */
+
+jQuery(document).ready(function(){
+
+  // Animate Box Shadow on some elements
+  // Add the overlay. We don't need it in HTML so we create it here
+  jQuery('.animate-on-hover .image').append('<span class="shadow-overlay hide-for-small"></span>');
+  jQuery('.otw-portfolio-item-link').append('<span class="shadow-overlay hide-for-small"></span>');
+
+  jQuery('.shadow-overlay').hover(function() {
+      jQuery(this).stop().stop().animate({boxShadow: '0 0 30px 0 rgba(0,0,0,0.7)'}, 300);
+  }, function(){
+      jQuery(this).stop().stop().animate({boxShadow: '0 0 0 0'}, 250);
+  });
+
+	// Clone portfolio items to get a second collection for Quicksand plugin
+	var $portfolioClone = jQuery(".otw-portfolio").clone();
+
+	// Attempt to call Quicksand on every click event handler
+	jQuery(".otw-portfolio-filter a").click(function(e){
+
+		jQuery(".otw-portfolio-filter li").removeClass("current");
+
+		// Get the class attribute value of the clicked link
+		var $filterClass = jQuery(this).parent().attr("class");
+
+		if ( $filterClass == "all" ) {
+			var $filteredPortfolio = $portfolioClone.find("li");
+		} else {
+			var $filteredPortfolio = $portfolioClone.find("li[data-type~=" + $filterClass + "]");
+		}
+
+		// Call quicksand
+		jQuery("ul.otw-portfolio").quicksand( $filteredPortfolio, {
+			duration: 500,
+			easing: 'easeInOutQuad'
+		});
+
+		jQuery(this).parent().addClass("current");
+
+		// Prevent the browser jump to the link anchor
+		e.preventDefault();
+	})
+
+});
